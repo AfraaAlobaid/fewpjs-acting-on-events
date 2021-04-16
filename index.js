7c42570734e9de8718306fdf6ea306046c212df7
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-04-16 15:17:50 +0930

    add event listener

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+let dodger = document.getElementById("dodger");
+document.addEventListener('keydown', (e) => {
+    if(e.key === 'ArrowLeft') moveDodgerLeft();
+    else if(e.key === 'ArrowRight') moveDodgerRight();
+})
+
+function moveDodgerLeft(){
+    let leftCoo = dodger.style.left.replace("px", "");
+    leftCoo = parseInt(leftCoo, 10);
+    dodger.style.left = (leftCoo > 0) ? `${leftCoo -1}px` : `${leftCoo}px`;
+}
+
+function moveDodgerRight(){
+    // let gameWidth = document.getElementById("game").offsetWidth;
+    // let dodgerWidth = dodger.offsetWidth;
+
+    let leftCoo = parseInt(dodger.style.left.replace("px", ""), 10);
+    dodger.style.left = (leftCoo < 360) ? `${leftCoo + 1}px` : `${leftCoo}px`;
+}
